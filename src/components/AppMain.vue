622056bd1833d2c94a894df446f0e5a55a6e4d3e
Riccardo Andreazza <riccardo.andreazza90@gmail.com>
2024-02-01 15:47:51 +0100

    add: card layout and project props

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,6 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
 
 export default {
     data() {
@@ -11,15 +12,26 @@ export default {
     created() {
         axios.get(`${this.serverUrl}/api/projects`)
         .then((resp) => {
-            console.log(resp);
+            this.projects = resp.data.results.data;
         })
+    },
+    components: {
+        ProjectCard
     }
 }
 </script>
 
 <template>
     <div class="container py-4 text-center">
+
         <h1 class="text-light">PROJECTS LIST</h1>
+
+        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-3">
+            <div v-for="project in projects" :key="project.id" class="col">
+                <ProjectCard :project="project"/>
+            </div>
+        </div>
+
     </div>
 </template>
 
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    data() {
+        return {
+            serverUrl: 'http://127.0.0.1:8000',
+        }
+    },
+    props: {
+        project: Object,
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">An item</li>
+            <li class="list-group-item">A second item</li>
+            <li class="list-group-item">A third item</li>
+        </ul>
+        <div class="card-body">
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
