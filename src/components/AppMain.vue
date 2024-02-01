d935962eb28aeae5334e4a071241028b26c953ee
Riccardo Andreazza <riccardo.andreazza90@gmail.com>
2024-02-01 16:56:03 +0100

    add: pagination

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,29 +6,53 @@ export default {
     data() {
         return {
             serverUrl: 'http://127.0.0.1:8000',
-            projects: []
+            projects: [],
+            page: 1,
+            lastPage: Number
         }
     },
     created() {
-        axios.get(`${this.serverUrl}/api/projects`)
-        .then((resp) => {
-            this.projects = resp.data.results.data;
-        })
+        this.apiCall();
     },
     components: {
         ProjectCard
+    },
+    methods: {
+        apiCall() {
+            axios.get(`${this.serverUrl}/api/projects`, {
+                params: {
+                    page: this.page
+                }
+            })
+                .then((resp) => {
+                    this.projects = resp.data.results.data;
+                    this.lastPage = resp.data.results.last_page;
+                })
+        },
+        nextPage() {
+            this.page++;
+            this.apiCall();
+        },
+        backPage() {
+            this.page--;
+            this.apiCall();
+        },
     }
 }
 </script>
 
 <template>
-    <div class="container py-4 text-center">
+    <div class="container-fluid p-5 text-center">
 
         <h1 class="text-light mb-5">PROJECTS LIST</h1>
+        <div class="d-flex justify-content-end mb-4 gap-4">
+            <button v-if="page > 1" @click="backPage" class="btn btn-success">BACK</button>
+            <button v-if="page != lastPage" @click="nextPage" class="btn btn-success">NEXT</button>
+        </div>
 
-        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-3">
+        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 g-3">
             <div v-for="project in projects" :key="project.id" class="col">
-                <ProjectCard :project="project"/>
+                <ProjectCard :project="project" />
             </div>
         </div>
 
