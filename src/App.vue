0d9249172f3b6d745948ef914298e8192771025a
Riccardo Andreazza <riccardo.andreazza90@gmail.com>
2024-02-01 15:24:44 +0100

    add: main component and link to App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script>
+import AppMain from './components/AppMain.vue'
 
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    AppMain
+  }
+}
 </script>
 
 <template>
-
+  <div class="container py-4">
+    <AppMain />
+  </div>
+  
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<template>
+    <h1 class="text-center">PROJECTS LIST</h1>
+</template>
+
+<style lang="scss" scoped></style>
